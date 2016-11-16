6cb5acacd43d123288ba0b5853e236382b28266b
Sam Caulfield <sam@samcaulfield.com>
2016-11-16 18:40:37 +0000

    Add Win32 support for timing functions
    The C standard library only provides second-resolution wall clock timing
    functions. On Linux the POSIX timing APIs are available to provide
    millisecond-resolution timing but these APIs are not available on
    Windows. Windows provides similar functions in the Win32 API.
    
    This commit adds a preprocessor check that causes the POSIX API to be
    used for timing when compiling for Linux and the Win32 API to be used
    when compiling for Windows.

--- time.c
+++ time.c
@@ -1,12 +1,17 @@
 #include <stdbool.h>
 #include <time.h>
 
+#ifdef _WIN32
+#include "Windows.h"
+#endif
+
 #include "time.h"
 
 #define MILLISECONDS_PER_SEC 1000
 #define MILLISECONDS_PER_NANOSECOND 1000000 /* million */
 #define NANOSECONDS_PER_SEC 1000000000L /* billion */
 
+#ifdef __linux__
 double milliseconds(struct timespec *ts)
 {
 	double s_to_ms = ts->tv_sec * MILLISECONDS_PER_SEC;
@@ -14,7 +19,6 @@ double milliseconds(struct timespec *ts)
 
 	return s_to_ms + ns_to_ms;
 }
-
 double getTimeSinceLastFrame()
 {
 	static struct timespec previousTime;
@@ -33,4 +37,28 @@ double getTimeSinceLastFrame()
 	previousTime = currentTime;
 	return ms / (double) MILLISECONDS_PER_SEC;
 }
+#elif defined _WIN32
+double milliseconds(SYSTEMTIME *time)
+{
+	// Prudent to include minutes in case a frame takes quite long
+	return time->wMinute * 60 * MILLISECONDS_PER_SEC + time->wSecond * MILLISECONDS_PER_SEC + time->wMilliseconds;
+}
+double getTimeSinceLastFrame()
+{
+	static SYSTEMTIME previousTime;
+	static bool timeInitialised = false;
+
+	if (!timeInitialised) {
+		GetSystemTime(&previousTime);
+		timeInitialised = true;
+	}
+
+	SYSTEMTIME currentTime;
+	GetSystemTime(&currentTime);
+
+	double ms = milliseconds(&currentTime) - milliseconds(&previousTime);
+	previousTime = currentTime;
+	return ms / (double) MILLISECONDS_PER_SEC;
+}
+#endif
 
