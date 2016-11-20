154209668d3009a9e1641a5cb980e821b958790c
Sam Caulfield <sam@samcaulfield.com>
2016-11-20 20:44:14 +0000

    Fix incorrect time unit in function description
    The time returned is in seconds, not milliseconds.
    This game engine uses SI units where reasonable.

--- time.h
+++ time.h
@@ -1,7 +1,7 @@
 #ifndef TIME_H
 #define TIME_H
 
-/* Returns the time since the previous call in milliseconds
+/* Returns the time since the previous call in seconds
  * For the first call, the return value will be zero
  * WARNING: May be unsuitable for very high frame rates as inter-frame time may
  * be too small to store
